ba7a032560bfcf7b6e9cdd88ef206c10c7c20b24
Тимофей Кузнецов <smile_kito@mail.ru>
2025-01-20 16:34:52 +1000

    add ef core to project, make dbcontext, add connection string to user secrets, up docker container with postgres image

---
+++ backend/src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PetFamily.Domain.PetContext.Entities;
+using PetFamily.Domain.SpeciesContext;
+
+namespace PetFamily.Infrastructure;
+
+public class ApplicationDbContext(IConfiguration configuration) : DbContext
+{
+    private const string DATABASE = "Postgres";
+    
+    public DbSet<Volunteer> Volunteers => Set<Volunteer>();
+    public DbSet<Species> Species => Set<Species>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE));
+    }
+}
