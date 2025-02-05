6d0dd9f6ca24cb56f0365754b4a83e2c5cb9f638
Тимофей Кузнецов <smile_kito@mail.ru>
2025-02-06 00:14:51 +1000

    add serial log and seq

--- backend/src/PetFamily.API/Program.cs
+++ backend/src/PetFamily.API/Program.cs
@@ -3,14 +3,31 @@ using PetFamily.API.Extensions;
 using PetFamily.API.Validation;
 using PetFamily.Application;
 using PetFamily.Infrastructure;
+using Serilog;
+using Serilog.Events;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") 
+                 ?? throw new ArgumentNullException("Seq connection string is null")) 
+    .Enrich.WithThreadId()
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithMachineName()
+    .Enrich.WithEnvironmentUserName()
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
+builder.Services.AddSerilog();
 
 builder.Services
     .AddInfrastructure()
@@ -19,6 +36,8 @@ builder.Services
 var app = builder.Build();
 
 app.UseExceptionMiddleware();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,17 +49,11 @@ if (app.Environment.IsDevelopment())
     await app.ApplyMigration();
 }
 
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 app.MapControllers();
-
-app.Use(async (context, next) =>
-{
-    Console.WriteLine(context.Request.Path);
-
-    await next.Invoke();
-});
-    
 
 app.Run();
