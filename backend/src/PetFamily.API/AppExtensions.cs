7e720bec9d9f19c07cb580d52e093fe7812cb568
Тимофей Кузнецов <smile_kito@mail.ru>
2025-02-05 16:36:15 +1000

    add auto migrations apply

---
+++ backend/src/PetFamily.API/AppExtensions.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PetFamily.Infrastructure;
+
+public static class AppExtensions
+{
+    public static async Task<WebApplication> ApplyMigration(this WebApplication app)
+    {
+        await using var scope = app.Services.CreateAsyncScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+
+        return app;
+    }
+}
