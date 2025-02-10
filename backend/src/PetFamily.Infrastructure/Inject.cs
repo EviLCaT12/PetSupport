d263c73036bb8c46bdeb7b3287ebe9e10e1bc900
Тимофей Кузнецов <smile_kito@mail.ru>
2025-02-10 15:41:27 +1000

    Preparatory work for using minio

--- backend/src/PetFamily.Infrastructure/Inject.cs
+++ backend/src/PetFamily.Infrastructure/Inject.cs
@@ -1,18 +1,41 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Minio;
 using PetFamily.Application.Volunteers;
+using PetFamily.Infrastructure.Options;
 using PetFamily.Infrastructure.Repositories;
 
 namespace PetFamily.Infrastructure;
 
 public static class Inject
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<ApplicationDbContext>();
-
         services.AddScoped<IVolunteersRepository, VolunteerRepository>();
-
+        services.AddMinio(configuration);
+        
         return services;
     }
-    
+
+    private static IServiceCollection AddMinio(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<MinioOptions>(
+            configuration.GetSection("Minio"));
+        
+        services.AddMinio(options =>
+        {
+            var minioOptions = configuration.GetSection(MinioOptions.MINIO).Get<MinioOptions>()
+                               ?? throw new ApplicationException("Missing " + MinioOptions.MINIO);
+            
+            options.WithEndpoint(minioOptions.Endpoint);
+
+            options.WithCredentials(minioOptions.AccessKey, minioOptions.SecretKey);
+
+            options.WithSSL(minioOptions.WithSsl);
+        });
+        return services;
+    }
 }
